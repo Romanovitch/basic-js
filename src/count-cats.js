5ac2efe0fe43f4d7cd9b77326b50b59fc97bba32
Romanovitch <rokos.job.ru@gmail.com>
2020-02-17 12:25:37 +0300

    feat: implement the countCats(backyard)

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,6 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  // return matrix.flat().filter(cat => cat === "^^").length // ???
+  return matrix.reduce((sum1, arr) => {
+    return sum1 + arr.reduce((sum2, el) => el === "^^" ? sum2 + 1 : sum2, 0)
+  }, 0)
 };
