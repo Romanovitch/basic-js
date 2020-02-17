56d565192a9be3d469402e653dc899b8d858a6b4
Romanovitch <rokos.job.ru@gmail.com>
2020-02-17 14:12:46 +0300

    feat: implement the function dateSample()

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(str) {
+  if ( (typeof(str) !== 'string') 
+    || isNaN(parseFloat(str))
+    || (!isNaN(str) && (+str <= 0 || +str > 15))){ 
+      return false
+  } 
+  return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(str)) 
+    / (0.693 / HALF_LIFE_PERIOD))
 };
